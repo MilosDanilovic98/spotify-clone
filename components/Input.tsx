8fcbbbb06a7e235a1656d60d83f3fe738118a8cd
Milos Danilovic <milos.danilovic@amplitudo.me>
2023-06-06 11:27:13 +0200

    feature:Finish song upload form and connected to database

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,10 +1,24 @@
 import React, { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, disabled, ...props }, ref) => {
-    return <input/>;
+    return (
+      <input
+        type={type}
+        className={twMerge(
+          "flex w-full rounded-md border bg-neutral-700 " +
+            "border-transparent px-3 py-3 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400" +
+            "focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        disabled={disabled}
+        ref={ref}
+        {...props}
+      />
+    );
   }
 );
 
